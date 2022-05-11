bf3d5eee821ca0e0e6ceff73c95b2a3f199ecbee
Lily Smart <hello@neyoa.me>
2022-05-11 14:10:01 +0100

    Add rust to fish startup script

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,20 +1,29 @@
+# No "Welcome to fish shell" on session start
 set fish_greeting
 
+# Use nano as the default text editor
 set -gx EDITOR nano
 
-if type -q exa # if exa is installed, use exa in place of ls
+# If exa is installed, use exa in place of ls, otherwise make aliases for ls
+if type -q exa
   alias ls "exa -F --icons"
   alias la "ls -a"
   alias ll "la -lh --no-filesize --no-time"
   alias lt "ls -T"
-else # otherwise make aliases for ls
+else
   alias ls "ls -p -G"
   alias la "ls -A"
   alias ll "ls -loA"
 end
 
+# Shortcut for git
 alias g git
 
+# Setup node version manager
 load_nvm > /dev/stderr
 
+# Setup rust
+set PATH $HOME/.cargo/bin $PATH
+
+# Setup starship prompt
 starship init fish | source
