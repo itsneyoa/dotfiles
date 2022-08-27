3f537f28f83d27e867de98293400d11eb25708ab
Lily Smart <hello@neyoa.me>
2022-08-27 18:47:42 +0600

    Custom fish colours

---
+++ config/fish/conf.d/theme.fish
@@ -0,0 +1,28 @@
+# Normal
+set fish_color_normal normal
+set fish_color_command brcyan
+set fish_color_quote green
+set fish_color_redirection magenta
+set fish_color_end bryellow
+set fish_color_keyword bryellow
+set fish_color_error red
+set fish_color_param brmagenta
+set fish_color_valid_path --underline
+set fish_color_option 
+set fish_color_comment 545454
+set fish_color_operator brwhite
+set fish_color_escape brwhite
+set fish_color_autosuggestion brblack
+
+# Overwritten by tide
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_user brgreen
+set fish_color_host normal
+
+# Pager
+set fish_pager_color_prefix yellow --bold --underline
+set fish_pager_color_completion yellow
+set fish_pager_color_description bryellow
+set fish_pager_color_selected_background --background=magenta
+set fish_pager_color_progress white --background=brmagenta
